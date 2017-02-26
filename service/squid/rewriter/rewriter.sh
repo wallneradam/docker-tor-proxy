34adaabd0135d61f6b1dcec90aa82fe2553a47fd
Adam Wallner <wallner@bitbaro.hu>
2017-02-26 08:21:06 +0100

    The rewriter script

---
+++ service/squid/rewriter/rewriter.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# It is a shell implementation of squid rewriter explained here:
+#  http://wiki.squid-cache.org/Features/Redirectors
+
+SED=''
+
+# Read SED data from config
+. /opt/squid/rewriter.conf
+
+while true; do
+    while IFS= read -r line; do
+        line_out=`echo "$line" | cut -f 1 -d " " | sed "${SED}"`
+        if [ "$line" != "$line_out" ]; then
+            echo "$line_out"
+        else
+            echo ""
+        fi
+    done
+done
